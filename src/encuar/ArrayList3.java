f70b3611c456bdebdad735c5f624c2603c17859d
super <super@super-portatil>
2019-03-20 08:46:26 +0100

    lina 7

--- src/encuar/ArrayList3.java
+++ src/encuar/ArrayList3.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class ArrayList3 {
 	public static void main(String[] args) {
+		int hola=0;
 		double old = System.currentTimeMillis();
 		ArrayList<Integer> AList = new ArrayList<Integer>();
 		for (int i = 0; i < 100000; i++) {
